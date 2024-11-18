3c40838cb1238c852b0bd74b15690608eb136889
ildar987 <blinov9313@gmail.com>
2024-11-18 22:47:11 +0600

    Add files via upload

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-all_the_tasks = 12
-hours_are_spent = 1.5
-course = 'Python'
-average_lead_time = 0.125
+all_the_tasks = 'all_the_tasks: 12'
+hours_are_spent = 'hours_are_spent: 1.5'
+course = 'course: Python'
+average_lead_time = 'average_lead_time 0.125 hour'
 print(course, all_the_tasks, hours_are_spent, average_lead_time)
