d210a425f340d468a5b6a6276713ad05b30cf28e
ildar987 <blinov9313@gmail.com>
2024-12-04 08:43:31 +0700

    Add files via upload

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-all_the_tasks = 'all_the_tasks: 12'
-hours_are_spent = 'hours_are_spent: 1.5'
-course = 'course: Python'
-average_lead_time = 'average_lead_time 0.125 hour'
-print(course, all_the_tasks, hours_are_spent, average_lead_time)
+all_the_tasks = '12'
+hours_are_spent = '1.5'
+course = 'Python'
+average_lead_time = '0.125'
+print(f' Курс: {course}, всего задач: {all_the_tasks}, затрачено часов: {hours_are_spent}, среднее время выполнения: {average_lead_time} часа.')
