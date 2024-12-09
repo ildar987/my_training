dbff6c061bae7cdd059f0495d5a9ed60f1d825fd
ildar987 <blinov9313@gmail.com>
2024-12-09 16:08:45 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_war = 1, 2, 3, 's', 'd', 'e'
+print(immutable_war)
+#immutable_war [1] = 'f'
+#print(immutable_war)   # кортеж не поддерживает обращение по элементам
+mutable_list = [4, 5, 6, 'q', 'w', 'r']
+mutable_list[1] = 'b'
+print(mutable_list)
